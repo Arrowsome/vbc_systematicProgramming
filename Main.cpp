877401c40ffd178ef8e173740670981b1d12992c
Arrowsome <quiver4arrow@gmail.com>
2024-01-19 14:57:55 +0200

    Add try-catch, Add file output

--- Main.cpp
+++ Main.cpp
@@ -6,16 +6,29 @@
 # include <random>
 # include <fstream>
 # include <sstream>
+# include <iterator>
 # include "Person.h"
+# include <chrono>
 using namespace std;
 
-#define MEASURE_TIME(code) \
+#define MEASURE_TIME(tag, code) \
 { \
-  auto start = std::chrono::high_resolution_clock::now (); \
-  code \
-  auto stop = std::chrono::high_resolution_clock::now (); \
-  std::chrono::duration<double, std::milli> ms = stop - start; \
-  std::cout << "Time taken: " << ms.count () << " ms\n"; \
+  auto start = chrono::high_resolution_clock::now (); \
+  bool success = true; \
+  try { \
+    code \
+  } \
+  catch (...) { \
+    success = false; \
+  } \
+  auto stop = chrono::high_resolution_clock::now (); \
+  chrono::duration<double, milli> ms = stop - start; \
+  if (success) { \
+    cout << #tag << ": time taken " << ms.count () << " ms\n"; \
+  } \
+  else { \
+    cout << #tag << ": time was not measured due to failure\n"; \
+  } \
 }
 
 int main() {
@@ -27,27 +40,35 @@ int main() {
     vector<Person> failedStudents;
 
     cout << "Enter homework final score calculation type (`a` for average, `m` for median): ";
-    string inputHwFinalScoreType;
-    cin >> inputHwFinalScoreType;
+    string hwFinalScoreTypeInput;
+    cin >> hwFinalScoreTypeInput;
     HomeworkFinalScoreType hwFinalScoreType;
-    if (inputHwFinalScoreType == "a") {
+    if (hwFinalScoreTypeInput == "a") {
         hwFinalScoreType = AVERAGE;
-    } else if (inputHwFinalScoreType == "m") {
+    } else if (hwFinalScoreTypeInput == "m") {
         hwFinalScoreType = MEDIAN;
     } else {
         throw invalid_argument("Homework final score calculation type should be either `a` (average) or `m` (median)");
     }
 
     ifstream file;
-    cout << "Measuring time for file load operation:\n";
-    MEASURE_TIME({
+    string input_filename;
+    try {
         if (fileNum == 1) {
-            file.open("Students10000.txt");
+            input_filename = "students10000.txt";
         } else if (fileNum == 2) {
-            file.open("Students100000.txt");
+            input_filename = "students100000.txt";
         } else {
             throw invalid_argument("File number should be in the acceptable range.");
-        }
+        }  
+    } catch(runtime_error &e) {
+        cout << "Error: " << e.what() << endl;
+    }
+      
+
+    cout << "Measuring time for file load operation:\n";
+    MEASURE_TIME(openning_file, {
+        file.open(input_filename);
     });
 
 
@@ -68,25 +89,49 @@ int main() {
         }
     }
 
-
+    // Output students to standard output
     string finalPointTitle;
-    if (hwFinalScoreType == AVERAGE) {
-        finalPointTitle = "Final_Point(Ave.)";
-    } else {
-        finalPointTitle = "Final_Point(Med.)";
-    }
+        if (hwFinalScoreType == AVERAGE) {
+            finalPointTitle = "Final_Point(Ave.)";
+        } else {
+            finalPointTitle = "Final_Point(Med.)";
+        }
     cout << left << setw(25) << "Name" <<  setw(25) << "Surname" << setw(25) << finalPointTitle << endl;
     cout << "------------------------------------------------------------------------------" << endl;
-    cout << "• Passed:\n";
-    for (int i = 0; i < passedStudents.size(); i++) {
-        cout << passedStudents[i] << endl;
-    }
 
-    cout << "• Failed:\n";
-    for (int i = 0; i < failedStudents.size(); i++) {
-        cout << failedStudents[i] << endl;
-    }
+    cout << "Measuring time for standard outputs:\n";
+    MEASURE_TIME(standard_output, {
+        cout << "• Passed:\n";
+        for (int i = 0; i < passedStudents.size(); i++) {
+            cout << passedStudents[i] << endl;
+        }
 
+        cout << "• Failed:\n";
+        for (int i = 0; i < failedStudents.size(); i++) {
+            cout << failedStudents[i] << endl;
+        }
+    });
+    
+
+    cout << "Measuring time for file outputs:\n";
+    MEASURE_TIME(file_output,{
+        // Output students vector to files
+        ofstream output_passed_file("./passed.txt");
+        for (int i = 0; i < passedStudents.size(); ++i) {
+            output_passed_file << passedStudents[i] << "\n";
+        }
+        cout << "File passed.txt was generated!" << endl;
+        output_passed_file.close();
+
+        ofstream output_failed_file("./failed.txt");
+        for (int i = 0; i < failedStudents.size(); ++i) {
+            output_failed_file << failedStudents[i] << "\n";
+        }
+        cout << "File failed.txt was generated!" << endl;
+        output_failed_file.close();
+    });
+
+    
     file.close();
 
     return 0;
